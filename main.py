32efdc90f7cccfb9b6bec80ee470ebfdfaf85853
mariu <bonillaalvarezmariajose5@gmail.com>
2026-09-03 20:44:24 +0000

    hola

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.db import init_db
 from app.routes import router
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_db()
-    yield
 
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router)
